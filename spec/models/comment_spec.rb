afda009e733ea1e7ff72e20ad00f72dbdcfd127c
Holger_Tidemand <tidemand.holger@gmail.com>
2018-02-24 12:08:12 +0100

    adds rspec test for comment class and migration

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+
+    it 'Should have a vaild factory' do
+      expect(FactoryBot.build(:comment)).to be_valid
+    end
+
+    it { is_expected.to have_db_column :id}
+    it { is_expected.to have_db_column :body}
+    it { is_expected.to have_db_column :email}
+
+
 end
