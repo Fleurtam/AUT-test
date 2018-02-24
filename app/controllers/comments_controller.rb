98d90197f5520a664c7b64a3de8f4de0b245faec
Holger_Tidemand <tidemand.holger@gmail.com>
2018-02-24 12:31:58 +0100

    adds comment to database and flash for success

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,12 @@
 class CommentsController < ApplicationController
   def create
     @comment = Comment.create(params_comment)
+    if @comment.persisted?
+      flash[:success] = 'Your comment has been succesfully created'
+      redirect_back(fallback_location: root_path)
+    end
   end
-
-
+  
   private
 
   def params_comment
